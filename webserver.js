a68fc215b3f74488ef09b24c06cd366746c3c0ce
seoseungwan145 <46492114+seoseungwan145@users.noreply.github.com>
2019-11-01 21:37:26 +0900

    Add files via upload

--- webserver.js
+++ webserver.js
@@ -1,6 +1,8 @@
 var http = require('http');
 var fs = require('fs');
 var socketio = require('socket.io');
+let {PythonShell} = require('python-shell');
+var async = require('async');
 
 var app = http.createServer(function(request,response){
     var url = request.url;
@@ -17,6 +19,11 @@ var app = http.createServer(function(request,response){
  
 }).listen(8000, function(){
     console.log('Server running at http://115.145.243.99:8000');
+    var optionss = { encoding: 'utf8', flag: 'w' };
+        
+        fs.writeFile('./output.csv', "", optionss, function(err){
+            console.log('출력 완료');
+        });
 });
 
 var io = socketio.listen(app);
@@ -24,19 +31,63 @@ io.sockets.on('connection', function (socket){
     console.log('Socket ID : ' + socket.id + ', Connect');
     socket.on('clientMessage', function(data){
         console.log('Client Message : ' + data);
-        
-        var output_data = data + '\n';
-        var options = { encoding: 'utf8', flag: 'a+' };
-        
-        fs.writeFile('./output.csv', output_data, options, function(err){
-            console.log('출력 완료');
 
-       var message = {
-            msg : 'server',
-            data : 'data'
-        };
-        socket.emit('serverMessage', message);
+        if(data != 12){
+            var output_data = data + '\n';
+            var options = { encoding: 'utf8', flag: 'a' };
+            
+            fs.writeFile('./output.csv', output_data, options, function(err){
+                console.log('출력 완료');
 
-        });
+            /*
+                var message = {
+                    msg : 'server',
+                    data : 'data'
+                };
+                socket.emit('serverMessage', message);
+                */
+            });
+
+        }else
+        {
+            async.waterfall([
+                function(callback) {
+                var options = {
+                    mode: 'text',
+                    pythonPath: '',
+                    pythonOptions: ['-u'],
+                    scriptPath: '',
+                    args: [1, 2, 3, 4, 5]
+                };
+
+                PythonShell.run('data_process.py',options,function(err,results){
+                    if(err) throw err;
+                    console.log('results: %j', results);
+                    callback(null, results);
+                });
+                }], function(callback, items){
+                    var optionss = {
+                        mode: 'text',
+                        pythonPath: '',
+                        pythonOptions: ['-u'],
+                        scriptPath: '',
+                        args: [1, 2, 3, 4, 5]
+                    };
+
+                    PythonShell.run('linear_regression.py',optionss,function(err,results){
+                        if(err) throw err;
+                        console.log('results: %j', results);
+                        var message = {
+                            msg : 'server',
+                            dust : results[0],
+                            temp : results[1],
+                            human : results[2],
+                            sunny : results[3],
+                            bias : results[4]
+                        };
+                        socket.emit('serverMessage', message); 
+                    });
+                });
+        }
     });
 });
